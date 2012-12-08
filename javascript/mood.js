aa7a70a0ac74cc76331626eb7efbc6b40d071094
Ember Baker <erb325@gmail.com>
2012-12-08 17:14:29 -0500

    SLider

--- javascript/mood.js
+++ javascript/mood.js
@@ -1,12 +1,5 @@
 var App = {
-  values : {
-    "angry" : "",
-    "happy" : "",
-    "sad" : "",
-    "party" : "",
-    "relaxing" : ""
-  },
-  
+  apiURL:"http://developer.echonest.com/api/v4/song/search?api_key=4LVJP0LNDBU3CDUS6&format=json&results=10&mood=",
   moods: {
     angry: ["aggressive", "angry", "angst-ridden", "complex", "dark", "disturbing", "harsh", "industrial", "intense", "manic", "rebellious", "strange"],
     happy: ["calming", "carefree", "cheerful", "cool", "fun", "futuristic", "gentle", "gleeful", "happy", "humorous", "joyous", "playful", "light", "lively", "sexy", "sweet", "theater", "warm", "whimsical"],
@@ -15,36 +8,53 @@ var App = {
     relaxing: ["ambient", "dreamy", "elegant", "intimate", "mediation", "passionate", "peaceful", "mystical", "mellow", "laid-back", "reflective", "relax", "romantic", "sexy", "smooth", "soothing", "sophisticated", "spacey", "spiritual"]
   }
 };
+App.getAllmoods = function(){
+    var moods = [];
+    $("#sliders > span").each(function(){
+        var type = $(this).attr("data");
+        var index = $(this).slider("value");
+        if(index == 0) {
+            return;
+        }
+        moods.push(App.moods[type][index]);
+    });
+    return moods.join();
+}
+App.searchMoods = function(terms){
+    console.log(App.apiURL+terms);
+    $.ajax(App.apiURL+terms).done(function(data){
+        App.showNames(data.response.songs);
+    });
 
+}
+App.showNames = function(songs){
+    if(songs == undefined || songs.length == 0) return;
+    for (var song in songs ) {
+        var li = $("<li/>").html(songs[song]['artist_name'] + ' - ' + songs[song]['title']);
+        $("#artists").append(li);
+
+
+    }
+
+};
 App.init = function() {
   $("#sliders > span").each(function() {
     // read initial values from markup and remove that
     var value = parseInt($(this).text(), 10);
-    $(this).empty().slider({
-      value: value,
+    console.log(value);
+    var slide = $(this);
+    slide.empty().slider({
+      value: 0,
       range: "min",
+      min: 0,
+      max: value-1,
       animate: true,
       orientation: "vertical",
-      slide: App.onChange
+      change:function(event, ui){
+         App.searchMoods(App.getAllmoods());
+      }
     });
+
   });
 
-}
-
-App.onChange = function() {
-  $("#sliders > span").each(function() {
-    var mood = $(this).attr('id');
-    var value = $(this).slider('value');
-    
-    App.values[mood] = value;
-    
-    
-  });
-  console.log(App.values);
-  /*
-  $('#info').empty()
-  $("#sliders > span").each(function() {
-    $('#info').append($(this).attr('id') + " - " + $(this).slider('value') + "<br/>");
-  });
-  */
-}
+};
