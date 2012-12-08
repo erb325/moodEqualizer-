d8b73ec6a1065c69f23b685324ba7448ef59d395
Ember Baker <erb325@gmail.com>
2012-12-08 12:06:03 -0500

    initial moods

---
+++ javascript/mood.js
@@ -0,0 +1,15 @@
+var app = {
+    moods : { angry: ["aggressive", "angry", "angest-ridden", "complex", "dark", "disturbing", "harsh", "industrial",
+                        "intense", "manic", "rebellious", "strange"],
+               happy : ["calming", "carefree", "cheerful", "cool", "fun", "futuristic", "gentle", "gleeful", "happy", "humorous",
+                         "joyous", "playful", "light", "lively", "sexy", "sweet", "theater", "warm", "whimsical"]    ,
+               sad: ["cold", "dramatic", "eerie", "gloomy", "haunting", "melancholia", "poignant", "ominous", "pastoral",
+                       "quiet", "sad", "sentimental", "strange"],
+               party : ["bouncy", "energetic", "enthusiastic", "epic", "fun", "funky", "groovy", "hyponic","party music",
+                        "rowdy", "theater", "trippy"] ,
+               relaxing: ["ambient", "dreamy", "elegant", "intimate", "mediation", "passionate", "peaceful", "mystical",
+                          "mellow", "laid-back","reflective","relax", "romantic", "sexy", "smooth","soothing", "sophisticated",
+                            "spacey", "spiritual"]
+                }
+
+}   ;
