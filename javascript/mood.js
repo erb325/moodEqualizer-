350d145843e5acdcb4e7e67a65d4dad581419eb8
Alex <SciutoAlex@gmail.com>
2012-12-08 12:55:28 -0500

    added jquery ui

--- javascript/mood.js
+++ javascript/mood.js
@@ -1,22 +1,12 @@
 var app = {
-    moods : { angry: ["aggressive", "angry", "angst-ridden", "complex", "dark", "disturbing", "harsh", "industrial",
-                        "intense", "manic", "rebellious", "strange"],
-               happy : ["calming", "carefree", "cheerful", "cool", "fun", "futuristic", "gentle", "gleeful", "happy", "humorous",
-                         "joyous", "playful", "light", "lively", "sexy", "sweet", "theater", "warm", "whimsical"]    ,
-               sad: ["cold", "dramatic", "eerie", "gloomy", "haunting", "melancholia", "poignant", "ominous", "pastoral",
-                       "quiet", "sad", "sentimental", "strange"],
-               party : ["bouncy", "energetic", "enthusiastic", "epic", "fun", "funky", "groovy", "hyponic","party music",
-                        "rowdy", "theater", "trippy"] ,
-               relaxing: ["ambient", "dreamy", "elegant", "intimate", "mediation", "passionate", "peaceful", "mystical",
-                          "mellow", "laid-back","reflective","relax", "romantic", "sexy", "smooth","soothing", "sophisticated",
-                            "spacey", "spiritual"]
-                },
-   mainMoods: function(){
-       return ["angry", "happy","sad", "party", "relaxing"];
-   }  ,
-    mood : function(moodName){
-
-        return this.moods [moodName];
-    }
-
+  moods: {
+    angry: ["aggressive", "angry", "angest-ridden", "complex", "dark", "disturbing", "harsh", "industrial", "intense", "manic", "rebellious", "strange"],
+    happy: ["calming", "carefree", "cheerful", "cool", "fun", "futuristic", "gentle", "gleeful", "happy", "humorous", "joyous", "playful", "light", "lively", "sexy", "sweet", "theater", "warm", "whimsical"],
+    sad: ["cold", "dramatic", "eerie", "gloomy", "haunting", "melancholia", "poignant", "ominous", "pastoral", "quiet", "sad", "sentimental", "strange"],
+    party: ["bouncy", "energetic", "enthusiastic", "epic", "fun", "funky", "groovy", "hyponic", "party music", "rowdy", "theater", "trippy"],
+    relaxing: ["ambient", "dreamy", "elegant", "intimate", "mediation", "passionate", "peaceful", "mystical", "mellow", "laid-back", "reflective", "relax", "romantic", "sexy", "smooth", "soothing", "sophisticated", "spacey", "spiritual"]
+  }
+  
+  init: function
+  
 };
